6cee2ae89131e5836c51a9b8317634d8bdbe4da7
Andrew Jo <andrewjo@gmail.com>
2015-01-12 14:57:05 -0800

    Generated by chef CLI

---
+++ metadata.rb
@@ -0,0 +1,13 @@
+name             'opsworks_rabbitmq'
+maintainer       'Andrew Jo'
+maintainer_email 'andrew@verdigris.co'
+license          'Simplified BSD'
+description      'Wrapper cookbook for installing and configuring RabbitMQ on AWS OpsWorks instances'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+%w(amazon debian ubuntu).each do |platform|
+  supports platform
+end
+
+depends 'rabbitmq', '~>3.7.0'
