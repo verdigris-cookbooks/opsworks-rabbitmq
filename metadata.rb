3aee46398502146bddbd9dd136e17ae871ebb161
Andrew Jo <andrewjo@gmail.com>
2015-01-13 19:10:36 -0800

    Bump to v0.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'andrew@verdigris.co'
 license          'Simplified BSD'
 description      'Wrapper cookbook for installing and configuring RabbitMQ on AWS OpsWorks instances'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.2.0'
 
 %w(amazon debian ubuntu).each do |platform|
   supports platform
